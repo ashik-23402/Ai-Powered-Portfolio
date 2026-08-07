f2c0350f83838a98ddae045dc3a8ac359f1c16bb
Md Ashikur Rahman <ashikur.exabyting@bkash.com>
2026-08-07 12:15:00 +0600

    scuffholding the ask me project

---
+++ src/main/java/com/ashik/askaboutme/AskaboutmeApplication.java
@@ -0,0 +1,13 @@
+package com.ashik.askaboutme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskaboutmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskaboutmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashik/askaboutme/AskaboutmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashik.askaboutme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskaboutmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
